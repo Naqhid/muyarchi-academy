361d7d1686af474dfbcc638240ca772303d12ad7
Mohammed Naqhid <mnaqhid@gmail.com>
2026-07-17 18:13:28 +0530

    Switch to HashRouter for GitHub Pages compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AuthProvider } from '@/hooks/use-auth'
 import { SettingsProvider } from '@/hooks/use-settings'
 import { ToastContextProvider } from '@/hooks/use-toast'
@@ -9,7 +9,7 @@ import '@/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/muyarchi-academy">
+    <HashRouter basename="/muyarchi-academy">
       <ToastContextProvider>
         <AuthProvider>
           <SettingsProvider>
@@ -17,6 +17,6 @@ createRoot(document.getElementById('root')!).render(
           </SettingsProvider>
         </AuthProvider>
       </ToastContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
