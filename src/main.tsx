eb052903319072131c1cbd407f93278465025317
Mohammed Naqhid <mnaqhid@gmail.com>
2026-07-17 18:18:42 +0530

    Remove basename from HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import '@/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter basename="/muyarchi-academy">
+    <HashRouter>
       <ToastContextProvider>
         <AuthProvider>
           <SettingsProvider>
